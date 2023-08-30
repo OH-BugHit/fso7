e9aa393df17670d5443c4e7f7a3003d6e8d41ba2
Olli Hilke <hilkolli@student.uef.fi>
2023-08-30 13:57:27 +0300

    country-hook kloonattu

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -156,6 +156,7 @@ const App = () => {
   const anecdoteById = (id) =>
     anecdotes.find(a => a.id === id)
 
+  // eslint-disable-next-line no-unused-vars
   const vote = (id) => {
     const anecdote = anecdoteById(id)
 
